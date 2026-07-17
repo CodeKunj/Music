b89b814ca6ba6d6cbac451372230be2b6da6cc8b
CodeKunj <kunjr1104@gmail.com>
2026-07-17 19:51:10 +0530

    feat: add script to fetch and log featured tracks from API

---
+++ scratch_fetch.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.get('http://localhost:5000/api/tracks/featured', (resp) => {
+  let data = '';
+  resp.on('data', (chunk) => { data += chunk; });
+  resp.on('end', () => { console.log(JSON.parse(data)); });
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
